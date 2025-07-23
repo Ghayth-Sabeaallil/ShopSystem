c79f13d9e46db122d1808f6ed83ac41fd16d5d8e
ghayth.sabeaallil <ghayth@luday.se>
2025-07-23 16:20:34 +0200

    add gh-pages and vite-plugin-rewrite-all dependencies; update homepage and deploy script

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/', // Replace 'my-vite-app' with your repository name
+  plugins: [react(), VitePluginRewriteAll()],
 })
